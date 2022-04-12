8952e37dc4bff1417bbecd6b33dddf3d9ea03fe1
Corrado.Ferra <corradoferra@gmail.com>
2022-04-12 10:11:14 +0200

    Specializations seeder

---
+++ database/seeds/SpecializationsTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Specialization;
+use Illuminate\Database\Seeder;
+
+class SpecializationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $Specializations = [
+            ["name" => "Allergy and immunology", "description" => ""],
+            ["name" => "Anesthesiology", "description" => ""],
+            ["name" => "Dermatology", "description" => ""],
+            ["name" => "Diagnostic radiology", "description" => ""],
+            ["name" => "Emergency medicine", "description" => ""],
+            ["name" => "Family medicine", "description" => ""],
+            ["name" => "Internal medicine", "description" => ""],
+            ["name" => "Medical genetics", "description" => ""],
+            ["name" => "Neurology", "description" => ""],
+            ["name" => "Nuclear medicine", "description" => ""],
+            ["name" => "Obstetrics and gynecology", "description" => ""],
+            ["name" => "Ophthalmology", "description" => ""],
+            ["name" => "Pathology", "description" => ""],
+            ["name" => "Pediatrics", "description" => ""],
+            ["name" => "Physical medicine and rehabilitation", "description" => ""],
+            ["name" => "Preventive medicine", "description" => ""],
+            ["name" => "Psychiatry", "description" => ""],
+            ["name" => "Radiation oncology", "description" => ""],
+            ["name" => "Surgery", "description" => ""],
+            ["name" => "Urology", "description" => ""],
+        ];
+        foreach ($Specializations as $Specialization) {
+            $newSpec = new Specialization();
+            $newSpec->name = $Specialization["name"];
+            $newSpec->description = $Specialization["description"];
+            $newSpec->save();
+        }
+    }
+}
