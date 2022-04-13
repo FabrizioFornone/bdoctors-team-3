b18f83bca42b7293a2fcd336bb50ebeea496d7b3
Lolpawer <dadu.petrosino@gmail.com>
2022-04-13 17:10:57 +0200

    compiles infocontroller crud

--- app/Http/Controllers/Admin/InfoController.php
+++ app/Http/Controllers/Admin/InfoController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Info;
+use App\Specialization;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class InfoController extends Controller
 {
@@ -14,7 +18,7 @@ class InfoController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
@@ -24,7 +28,9 @@ class InfoController extends Controller
      */
     public function create()
     {
-        //
+        $users = User::where("id", Auth::user()->id)->get();
+
+        return view('admin.infos.create', compact('users'));
     }
 
     /**
@@ -35,7 +41,23 @@ class InfoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required|min:3',
+            'surname' => 'required|min:3',
+            'CV' => 'nullable|file|max:500',
+            'photo' => 'nullable|image|max:500',
+            'address' => 'nullable|min:5',
+            'phone' => 'nullable|min:11|numeric'
+        ]);
+
+        $info = new Info();
+        $info->fill($data);
+
+        $info->user_id = Auth::user()->id;
+
+        $info->save();
+
+        return redirect()->route('');
     }
 
     /**
@@ -46,7 +68,9 @@ class InfoController extends Controller
      */
     public function show($id)
     {
-        //
+        $info = Info::all();
+
+        return view('', compact('info'));
     }
 
     /**
@@ -57,7 +81,10 @@ class InfoController extends Controller
      */
     public function edit($id)
     {
-        //
+        $info = Info::findOrFail('id');
+        $specialization = Specialization::all();
+
+        return view('', compact('info', 'specialization'));
     }
 
     /**
@@ -69,7 +96,20 @@ class InfoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required|min:3',
+            'surname' => 'required|min:3',
+            'CV' => 'nullable|file|max:500',
+            'photo' => 'nullable|image|max:500',
+            'address' => 'nullable|min:5',
+            'phone' => 'nullable|min:11|numeric'
+        ]);
+
+        $info = Info::findOrFail($id);
+
+        $info->update($data);
+
+        return redirect()->route('', $id);
     }
 
     /**
