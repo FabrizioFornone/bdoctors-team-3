3137cdbedaf6873f8970f485e03524f2abe14b19
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-04-15 12:44:31 +0200

    styling index.blade.php

--- resources/views/admin/infos/index.blade.php
+++ resources/views/admin/infos/index.blade.php
@@ -5,39 +5,49 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header d-flex">
-                    Test
-                    
-                    @if ($boolean)
-                        <a class="ms-auto" href="{{ route('admin.infos.create') }}">
-                            <i class="fa-solid fa-plus"></i>
-                            Create
-                        </a>
-                        @else
-                        @foreach ( $infos as $info)
-                        <a class="ms-auto" href="{{ route('admin.infos.edit', $info->id) }}">
-                            <i class="fa-solid fa-plus"></i>
-                            Edit
-                        </a>
-                        @endforeach
-                    @endif
+                <div class="card-header text-center">
+                    <h4>Personal Informations</h4>
                 </div>
+                @foreach ( $infos as $info)
 
                 <div class="card-body">
-                    ciao
-                    @foreach ( $infos as $info )
-                    <div>
-                       Sto prendendo dalla tabella "infos": {{$info->phone}}
+                    <div class="d-flex my-2">
+                        {{-- Profile IMG --}}
+                        <div>
+                            <img class="w-25" src="{{asset('storage/' . $info->photo) }}" alt="">
+                        </div>
+                        @foreach ( $users as $user )
+                        <div>
+                            <h3>
+                                {{$user->name}} {{$user->surname}}
+                            </h3>
+                            @if ($boolean)
+                            <button type="button" class="btn btn-primary">
+                                <a class="ms-auto text-white text-decoration-none"
+                                    href="{{ route('admin.infos.create') }}">
+                                    <i class="fa-solid fa-plus"></i>
+                                    Create
+                                </a>
+                            </button>
+
+                            @else
+                            <button type="button" class="btn btn-primary">
+                                <a class="ms-auto text-white text-decoration-none"
+                                    href="{{ route('admin.infos.edit', $info->id) }}">
+                                    <i class="fa-solid fa-plus"></i>
+                                    Edit
+                                </a>
+                            </button>
+                            @endif
+
+                        </div>
+                        @endforeach
                     </div>
+
 
                     @endforeach
 
-                    @foreach ( $users as $user )
-                    <div>
-                      sto prendendo dalla tabella "users": {{$user->name}}
-                    </div>
 
-                    @endforeach
                 </div>
             </div>
         </div>
