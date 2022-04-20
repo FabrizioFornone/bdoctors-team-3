d2e1f3c2c4b29b457750c161277912c5e41ec84b
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-04-20 11:40:47 +0200

    footer draft

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,47 @@
-<h1>test</h1>
+<footer class="fixed-bottom">
+    <div class="container d-flex justify-content-between">
+        <div class="d-flex list-container">
+            <div>
+                <h4>DC Comics</h4>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>DC</h4>
+                <ul>
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy policy (New)</a></li>
+                    <li><a href="#">Ad Choice</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>SITES</h4>
+                <ul>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">Dc Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <img class="logo" src="{{asset('imgs/logo.png')}}" alt="">
+        </div>
+    </div>  
+</footer>
