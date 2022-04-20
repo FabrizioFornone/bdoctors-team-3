32deb3d1a7cfaa9fbfaef15414bd31da79a78143
Cezar <cezarlantosgmail.com>
2022-04-20 15:42:39 +0200

    vue config + mix pack

---
+++ resources/js/router.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "./pages/Home.vue";
+import Error from "./pages/Error.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: "history",
+
+    routes: [
+        { path: "/", component: Home, name: "home.index",
+            meta: {
+                title: "Homepage",
+                linkText: "Home"
+            }
+        },
+
+        {
+            path: "*", component:Error, name: "error"
+        }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title;
+
+    next();
+});
+
+export default router;
