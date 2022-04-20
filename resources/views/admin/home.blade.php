2200cba5ca5f7dcb86b92d62f93c01f951bca1d1
Lolpawer <dadu.petrosino@gmail.com>
2022-04-20 16:40:49 +0200

    adds first part of dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,19 +5,21 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header text-center fs-3 fw-bold">Dashboard</div>
 
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
+                <div class="card-body text-center">
+                    <div class="d-flex justify-content-between ms_card-div mx-auto">
                         @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
-
-                        <a href="{{ route('admin.infos.index') }}">Area Personale</a>
+                        <a href="{{ route('admin.infos.index') }}" class="btn btn-primary text-white fw-bold">PERSONAL AREA</a>
+                        <a href="{{ route('admin.reviews.index') }}" class="btn btn-primary text-white fw-bold">REVIEWS</a>
+                        <a href="{{ route('admin.messages.index') }}" class="btn btn-primary text-white fw-bold">MESSAGES</a>
+                        <a class="btn btn-primary text-white fw-bold">ADVERTISING</a>
+                       
 
                     </div>
 
