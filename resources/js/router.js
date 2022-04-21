a5080bdd9f3de2d0c1e528b48134ce07f4fa77b2
RooRsmoka <chri.disa96@gmail.com>
2022-04-21 11:00:45 +0200

    adds routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "./pages/Home.vue";
 import Error from "./pages/Error.vue";
+import AdvancedSearch from "./pages/AdvancedSearch.vue";
 
 Vue.use(VueRouter);
 
@@ -9,15 +10,29 @@ const router = new VueRouter({
     mode: "history",
 
     routes: [
-        { path: "/", component: Home, name: "home.index",
+        { 
+            path: "/", 
+            component: Home, 
+            name: "home.index",
             meta: {
                 title: "Homepage",
                 linkText: "Home"
             }
         },
-
         {
-            path: "*", component:Error, name: "error"
+            path: "/search",
+            component: AdvancedSearch,
+            name: "Search.index",
+            meta: {
+                title: "Advanced Search",
+                linkText: "Search"
+            }
+        },
+        {
+            path: "/not-found",
+            alias: "*", 
+            component: Error, 
+            name: "error"
         }
     ]
 });
