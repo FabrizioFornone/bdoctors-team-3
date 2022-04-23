a2683accfa53065e065d7164d6129801259241cf
RooRsmoka <chri.disa96@gmail.com>
2022-04-23 11:00:13 +0200

    creation info card structure

--- resources/js/components/InfoCard.vue
+++ resources/js/components/InfoCard.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-        <a href=""> {{ specialization.name }}</a>
+    <div class="col">
+        <div class="card">
+            <img :src="specialization.photo" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">Dettagli Dottore</h5>
+                <span> {{ specialization.name }} {{ specialization.surname }}</span>
+                <br/>
+                <div>
+                    <span></span>
+                </div>
+                <div>
+                    <span></span>
+                </div>
+            </div>
+            <div class="card-footer">
+                <!-- <router-link :to="{ name:'post.show', params: { post: post.slug } }">Dettagli</router-link> -->
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,16 +24,27 @@
 
 export default {
     props: {
-        specialization:Object
+        specialization: Object
     },
 
-    data() {
-        return {};
-    },
+    methods: {
+        // getInfoCardImg(specialization) {
+        //     return (
+        //         specialization.photo ??
+        //         "https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png"
+        //     );
+        // }
+    }
 }
 
 </script>
 
 <style lang="scss" scoped>
-    
+.post-card {
+    min-height: 100%;
+    .card-img-top {
+        max-height: 250px;
+        object-fit: cover;
+    }
+}
 </style>
