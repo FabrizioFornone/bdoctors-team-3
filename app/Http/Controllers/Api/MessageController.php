f2313ae7efa2bb40e3ab1316b4f2f429cab6a2f1
Cezar <cezarlantosgmail.com>
2022-04-25 12:00:40 +0200

    create message controller

---
+++ app/Http/Controllers/Api/MessageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function store(Request $request) {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ]);
+        $message = new Message();
+        $message->fill($data);
+        $message->save();
+        return response()->json($message);
+    }
+}
