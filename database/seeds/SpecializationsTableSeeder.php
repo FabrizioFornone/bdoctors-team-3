a9a66222fdcba1d3bfb91b3ae8e08e85636dc15e
RooRsmoka <chri.disa96@gmail.com>
2022-04-25 10:26:15 +0200

    quick fix in seeder specializations

--- database/seeds/SpecializationsTableSeeder.php
+++ database/seeds/SpecializationsTableSeeder.php
@@ -14,30 +14,30 @@ class SpecializationsTableSeeder extends Seeder
     public function run()
     {
         $Specializations = [
-            ["name" => "Allergy and immunology", "description" => ""],
-            ["name" => "Anesthesiology", "description" => ""],
-            ["name" => "Dermatology", "description" => ""],
-            ["name" => "Diagnostic radiology", "description" => ""],
-            ["name" => "Emergency medicine", "description" => ""],
-            ["name" => "Family medicine", "description" => ""],
-            ["name" => "Internal medicine", "description" => ""],
-            ["name" => "Medical genetics", "description" => ""],
-            ["name" => "Neurology", "description" => ""],
-            ["name" => "Nuclear medicine", "description" => ""],
-            ["name" => "Obstetrics and gynecology", "description" => ""],
-            ["name" => "Ophthalmology", "description" => ""],
-            ["name" => "Pathology", "description" => ""],
-            ["name" => "Pediatrics", "description" => ""],
-            ["name" => "Physical medicine and rehabilitation", "description" => ""],
-            ["name" => "Preventive medicine", "description" => ""],
-            ["name" => "Psychiatry", "description" => ""],
-            ["name" => "Radiation oncology", "description" => ""],
-            ["name" => "Surgery", "description" => ""],
-            ["name" => "Urology", "description" => ""],
+            ["specialization_name" => "Allergy and immunology", "description" => ""],
+            ["specialization_name" => "Anesthesiology", "description" => ""],
+            ["specialization_name" => "Dermatology", "description" => ""],
+            ["specialization_name" => "Diagnostic radiology", "description" => ""],
+            ["specialization_name" => "Emergency medicine", "description" => ""],
+            ["specialization_name" => "Family medicine", "description" => ""],
+            ["specialization_name" => "Internal medicine", "description" => ""],
+            ["specialization_name" => "Medical genetics", "description" => ""],
+            ["specialization_name" => "Neurology", "description" => ""],
+            ["specialization_name" => "Nuclear medicine", "description" => ""],
+            ["specialization_name" => "Obstetrics and gynecology", "description" => ""],
+            ["specialization_name" => "Ophthalmology", "description" => ""],
+            ["specialization_name" => "Pathology", "description" => ""],
+            ["specialization_name" => "Pediatrics", "description" => ""],
+            ["specialization_name" => "Physical medicine and rehabilitation", "description" => ""],
+            ["specialization_name" => "Preventive medicine", "description" => ""],
+            ["specialization_name" => "Psychiatry", "description" => ""],
+            ["specialization_name" => "Radiation oncology", "description" => ""],
+            ["specialization_name" => "Surgery", "description" => ""],
+            ["specialization_name" => "Urology", "description" => ""],
         ];
         foreach ($Specializations as $Specialization) {
             $newSpec = new Specialization();
-            $newSpec->name = $Specialization["name"];
+            $newSpec->specialization_name = $Specialization["specialization_name"];
             $newSpec->description = $Specialization["description"];
             $newSpec->save();
         }
