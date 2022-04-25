476b06a5c20a8e36a19e4e357c3872f7432c3876
Cezar <cezarlantosgmail.com>
2022-04-25 12:01:45 +0200

    create message and review pages in vue

---
+++ resources/js/pages/Message.vue
@@ -0,0 +1,82 @@
+<template>
+<div>
+    <h1>Send an message to your doctor</h1>
+    <div v-if="!formSubmitted">
+        <div class="mb-3">
+            <label for="exampleFormControlInput2" class="form-label">
+                Name
+            </label>
+            <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="Mario Rossi" v-model="formData.name"/>
+            <span class="text-danger" v-if="formValidationErrors && formValidationErrors.name">
+                {{ formValidationErrors.name }}
+            </span>
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">
+                Email address
+            </label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" v-model="formData.email"/>
+            <span class="text-danger" v-if="formValidationErrors && formValidationErrors.email">
+                {{ formValidationErrors.email }}
+            </span>
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">
+                Message
+            </label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" v-model="formData.message"></textarea>
+            <span class="text-danger" v-if="formValidationErrors && formValidationErrors.message">
+                {{ formValidationErrors.message }}
+            </span>
+        </div>
+        
+        <div>
+            <button type="submit" class="btn btn-primary text-white mb-3" @click="formSubmit">
+                Submit
+            </button>
+        </div>
+    </div>
+    <div v-else class="alert alert-success py-5">
+        <h4>Your message was sent successfully.</h4>
+    </div>
+</div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            formSubmitted: false,
+            formData: {
+                name: '',
+                email: '',
+                message: '',
+            },
+            formValidationErrors: null
+        }
+    },
+    methods: {
+        async formSubmit() {
+            try {
+                this.formValidationErrors = null;
+                const formDataInstance = new FormData();
+                formDataInstance.append('name', this.formData.name);
+                formDataInstance.append('email', this.formData.email);
+                formDataInstance.append('message', this.formData.message);
+                const resp = await axios.post('/api/messages', formDataInstance);
+                resp.data;
+                this.formSubmitted = true;
+            } catch (error) {
+                alert("Try again! Your message wasn't sent successfully or the fields are not compiled correctly.");
+            }
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
