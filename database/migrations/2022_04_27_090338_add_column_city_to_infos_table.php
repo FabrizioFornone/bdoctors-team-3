66276e69ee1d84f0dce0a23e43fd0807883dbda4
RooRsmoka <chri.disa96@gmail.com>
2022-04-27 11:32:25 +0200

    add column to infos table and quick fix

--- database/migrations/2022_04_27_090338_add_column_city_to_infos_table.php
+++ database/migrations/2022_04_27_090338_add_column_city_to_infos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnAddressInUsersTable extends Migration
+class AddColumnCityToInfosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class AddColumnAddressInUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('business_address')->after('surname');
-            
+        Schema::table('infos', function (Blueprint $table) {
+            $table->string('city')->after('address');
         });
     }
 
@@ -26,8 +25,8 @@ class AddColumnAddressInUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('business_address');
+        Schema::table('infos', function (Blueprint $table) {
+            $table->dropColumn('city');
         });
     }
 }
