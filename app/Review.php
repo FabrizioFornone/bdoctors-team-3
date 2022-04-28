83fdb5dfb019e0a7c4071c5cb2eefd4d72deae9f
RooRsmoka <chri.disa96@gmail.com>
2022-04-28 17:25:30 +0200

    add user_id in fillable

--- app/Review.php
+++ app/Review.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    protected $fillable = ['full_name', 'email', 'text', 'vote'];
+    protected $fillable = ['full_name', 'email', 'text', 'vote', 'user_id'];
 
     public function user() 
     {
