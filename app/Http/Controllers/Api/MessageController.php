a5770b215487cd734e851cc132e7c740147f08d3
Cezar <cezarlantosgmail.com>
2022-05-02 17:23:11 +0200

    fixes message and review controller

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -10,7 +10,7 @@ class MessageController extends Controller
 {
     public function store(Request $request) {
         $data = $request->validate([
-            'full_name' => 'required|alpha',
+            'full_name' => 'required',
             'email' => 'required|email',
             'message' => 'required|string',
             'user_id' => 'required',
