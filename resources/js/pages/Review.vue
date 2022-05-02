ab8b262d48466b744fb4c4e450e452ac1f2197ce
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-05-02 16:46:47 +0200

    messages and reviews style changed

--- resources/js/pages/Review.vue
+++ resources/js/pages/Review.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <div class="container">
+        <div class="container py-5">
             <!-- Back Arrow -->
             <a href="javascript:history.back()">
                 <i class="fa-solid fa-arrow-left ms_back-arrow mt-3"></i>
