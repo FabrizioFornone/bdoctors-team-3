8b20a11729aca379afaef168955721823441e125
Lolpawer <dadu.petrosino@gmail.com>
2022-05-02 17:02:24 +0200

    adds brief description of performances

--- resources/views/admin/infos/create.blade.php
+++ resources/views/admin/infos/create.blade.php
@@ -123,6 +123,8 @@
                         {{-- Performances --}}
                         <div class="mb-3">
                             <label>Performances</label>
+                            </br>
+                            <label>Write what kind of services you'll be providing patients</label>
                             <input type="text" name="performances"
                                 class="form-control @error('performances') is-invalid @enderror"
                                 placeholder="{{$user->performances}}" value="{{ old('performances') }}" required>
